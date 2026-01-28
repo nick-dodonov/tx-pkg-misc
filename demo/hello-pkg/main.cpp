5011252ef8cdd50a481cf81b16b00acfa1ee39c9
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2026-01-28 21:05:06 +0100

    Add build-time variable rule and log variable values in hello-pkg

--- demo/hello-pkg/main.cpp
+++ demo/hello-pkg/main.cpp
@@ -1,7 +1,20 @@
 #include "Boot/Boot.h"
+#include "Log/Log.h"
+#include "Fs/Drive.h"
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
 
 int main(int argc, const char** argv)
 {
     Boot::LogHeader({argc, argv});
+
+    Log::Info("FOO: {}", STR(FOO));
+    Log::Info("WORKSPACE_NAME: {}", STR(WORKSPACE_NAME));
+    Log::Info("LABEL_NAME: {}", STR(LABEL_NAME));
+    Log::Info("LABEL_PACKAGE_NAME: {}", STR(LABEL_PACKAGE_NAME));
+    Log::Info("LABEL_WORKSPACE_NAME: {}", STR(LABEL_WORKSPACE_NAME));
+    Log::Info("LABEL_REPO_NAME: {}", STR(LABEL_REPO_NAME));
+    Log::Info("LABEL_WORKSPACE_ROOT: {}", STR(LABEL_WORKSPACE_ROOT));
     return argc - 1;
 }
