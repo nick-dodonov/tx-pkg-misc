e478b7d99da76e48e0c5a3fbf2144fdbf3649b0a
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2026-01-29 21:31:57 +0100

    try demos for http moved to aux module (closer to usage)

--- demo/try-http-nghttp2/Http2Client.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Http1Client.h"
-#include <string>
-
-/// Make HTTP/2 request using nghttp2
-HttpResponse makeHttp2Request(const std::string& host, int port, const std::string& path);
